52fc04bacf7e84aa37701fb05a71c7537b9054c2
Олеся Максакова <olesa@macbook-pro-olesya.local>
2017-02-13 12:55:37 +0300

    Initial Commit

---
+++ YevgeniaApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  YevgeniaApp
+//
+//  Created by Олеся Максакова on 13.02.17.
+//  Copyright © 2017 HSEProject. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
